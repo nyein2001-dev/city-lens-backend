b2f56e1a599c62a1581b97b18b8e73147644fd35
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-07-31 00:09:10 +0630

    Initial commit

--- db_config.py
+++ db_config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://geo_user:geo_password@localhost/geovisualization'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://geo_user:geo_password@localhost/geovisualization')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
