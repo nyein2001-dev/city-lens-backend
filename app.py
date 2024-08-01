8612ad1b82f96cae087effbdafb2b0196cfcebb7
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-08-01 10:32:24 +0630

    Fix: Added psycopg2 and updated database URI

--- app.py
+++ app.py
@@ -1,24 +1,13 @@
-import os
 from flask import Flask
-from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from db_config import Config
-from dotenv import load_dotenv
-
-load_dotenv()
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://u1f1ppkqdof6rb:p63cc0a4c9d8ba05114ab4eac7f77c30c39ae05a7e1dd2c806485a0df98c51a2a@c67okggoj39697.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/db062dgbvu3ma4'
 db = SQLAlchemy(app)
-CORS(app)
 
-env_config = os.getenv("PROD_APP_SETTINGS", "config.DevelopmentConfig")
-app.config.from_object(env_config)
-
-from routes import *
-
-with app.app_context():
-    db.create_all()
+@app.route('/')
+def home():
+    return "Hello, World!"
 
 if __name__ == '__main__':
     app.run(debug=True)
