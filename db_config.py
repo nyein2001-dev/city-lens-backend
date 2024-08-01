2122687b447544504b6ce87f0a3117526f653d7b
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-08-01 10:17:03 +0630

    Fix database URI scheme

--- db_config.py
+++ db_config.py
@@ -1,6 +1,8 @@
 import os
-# export DATABASE_URL=postgresql://geo_user:geo_password@localhost/geovisualization
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://geo_user:geo_password@localhost/geovisualization')
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URL',
+        'postgresql://geo_user:geo_password@localhost/geovisualization'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
