01714918ba30aff65573295230613a66d192fdbe
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-09-03 11:43:59 +0630

    update api

--- dashboard/serializers.py
+++ dashboard/serializers.py
@@ -5,13 +5,30 @@ from .models import Stop, Route
 class StopSerializer(serializers.ModelSerializer):
     class Meta:
         model = Stop
-        fields = '__all__'  # or list specific fields
+        fields = '__all__'
 
 class RouteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Route
-        fields = '__all__'  # or list specific fields
+        fields = '__all__'
 
+    def to_representation(self, instance):
+        request = self.context.get('request')
+        if request and request.method == 'GET':
+            if request.parser_context['kwargs'].get('pk'):
+                self.fields = {
+                    'shape': serializers.JSONField(),
+                    'stops': serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+                }
+            else:
+                self.fields = {
+                    'id': serializers.IntegerField(),
+                    'route_id': serializers.CharField(),
+                    'name': serializers.CharField(),
+                    'agency_id': serializers.CharField(),
+                    'color': serializers.CharField(),
+                }
+        return super(RouteSerializer, self).to_representation(instance)
 
 class GeolocationSerializer(serializers.ModelSerializer):
     class Meta:
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, serializers
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Geolocation
 from .serializers import GeolocationSerializer
@@ -10,14 +10,33 @@ from .serializers import StopSerializer, RouteSerializer
 class StopViewSet(viewsets.ModelViewSet):
     queryset = Stop.objects.all()
     serializer_class = StopSerializer
-    pagination_class = CustomPageNumberPagination  # Optional, if pagination is needed
-    filter_backends = (DjangoFilterBackend,)  # Optional, if filtering is needed
+    pagination_class = CustomPageNumberPagination
+    filter_backends = (DjangoFilterBackend,)
+
+class RouteListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Route
+        fields = ['id', 'route_id', 'name', 'agency_id', 'color']
+
+class RouteDetailSerializer(serializers.ModelSerializer):
+    stops = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Route
+        fields = ['shape', 'stops']
 
 class RouteViewSet(viewsets.ModelViewSet):
     queryset = Route.objects.all()
-    serializer_class = RouteSerializer
-    pagination_class = CustomPageNumberPagination  # Optional, if pagination is needed
-    filter_backends = (DjangoFilterBackend,)  # Optional, if filtering is needed
+    pagination_class = CustomPageNumberPagination
+    filter_backends = (DjangoFilterBackend,)
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return RouteListSerializer
+        if self.action == 'retrieve':
+            return RouteDetailSerializer
+        return RouteSerializer
+
 
 class GeolocationViewSet(viewsets.ModelViewSet):
     queryset = Geolocation.objects.all()
